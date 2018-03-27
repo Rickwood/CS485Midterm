7e6ab98787c8ff5337962952e1b630288b07240e
Rickwood <32183354+Rickwood@users.noreply.github.com>
2018-03-27 13:13:14 -0700

    'log

---
+++ Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private bool gameOver = false;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+    private void Update()
+    {
+        if (gameOver == true)
+            GameOver();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "YOU WIN!!!  YOU'RE SO AMAZING OMG I CAN'T BELIEVE YOU BEAT THE HARDEST GAME OF ALL TIME!!!!!!!!!! \r\n Press 'Esc' to return to the Menu";
+            GameObject.FindGameObjectWithTag("EndSong").GetComponent<AudioSource>().Play();
+            gameOver = true;
+        }
+    }
+
+    void GameOver()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
