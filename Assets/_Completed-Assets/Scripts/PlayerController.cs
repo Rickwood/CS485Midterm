82cd7a4c9f48556baf604e637048bdf5400b9d22
Rickwood <32183354+Rickwood@users.noreply.github.com>
2018-03-27 20:47:22 -0700

    'log

--- Assets/_Completed-Assets/Scripts/PlayerController.cs
+++ Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public bool isGameOver = false;
 
     private Rigidbody2D rb2d;
     private int count;
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour {
 
     private void Start()
     {
+        GameObject.FindGameObjectWithTag("Background").GetComponent<AudioSource>().Play();
         rb2d = GetComponent<Rigidbody2D>();
         count = 0;
         SetCountText();
@@ -27,13 +29,24 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
+
+        if (gameOver == false)
+            rb2d.AddForce(movement * speed);
+        else
+        {
+            rb2d.velocity = Vector3.zero;
+            rb2d.velocity = Vector3.zero;
+        }
     }
 
     private void Update()
     {
         if (gameOver == true)
+        {
+            rb2d.velocity = Vector3.zero;
+            rb2d.velocity = Vector3.zero;
             GameOver();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -45,6 +58,14 @@ public class PlayerController : MonoBehaviour {
             count = count + 1;
             SetCountText();
         }
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            GameObject.FindGameObjectWithTag("Background").GetComponent<AudioSource>().Stop();
+            GameObject.FindGameObjectWithTag("Hazard").GetComponent<AudioSource>().Play();
+            gameOver = true;
+            isGameOver = true;
+            winText.text = "You Lose!!  Don't touch the boulders to survive!  Please try again!! \r\n Press 'Esc' to return to the Menu";
+        }
     }
 
     void SetCountText()
@@ -52,9 +73,11 @@ public class PlayerController : MonoBehaviour {
         countText.text = "Count: " + count.ToString();
         if (count >= 12)
         {
+            GameObject.FindGameObjectWithTag("Background").GetComponent<AudioSource>().Stop();
             winText.text = "YOU WIN!!!  YOU'RE SO AMAZING OMG I CAN'T BELIEVE YOU BEAT THE HARDEST GAME OF ALL TIME!!!!!!!!!! \r\n Press 'Esc' to return to the Menu";
             GameObject.FindGameObjectWithTag("EndSong").GetComponent<AudioSource>().Play();
             gameOver = true;
+            Vector2 movement = new Vector2(0, 0);
         }
     }
 
